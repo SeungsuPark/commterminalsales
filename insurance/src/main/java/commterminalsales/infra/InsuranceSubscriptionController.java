0025722e88e7da76cec15d1f009b3ada73d84776
Jinyoung Jang <jyjang@uengine.org>
2024-10-01 17:12:52 +0900

    commit

--- insurance/src/main/java/commterminalsales/infra/InsuranceSubscriptionController.java
+++ insurance/src/main/java/commterminalsales/infra/InsuranceSubscriptionController.java
@@ -1,32 +1,23 @@
 package commterminalsales.infra;
 
-import commterminalsales.domain.InsuranceSubscription;
-import commterminalsales.domain.InsuranceSubscriptionRepository;
+import commterminalsales.domain.*;
+import java.util.Optional;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+
+//<<< Clean Arch / Inbound Adaptor
 
 @RestController
+// @RequestMapping(value="/insuranceSubscriptions")
+@Transactional
 public class InsuranceSubscriptionController {
 
     @Autowired
     InsuranceSubscriptionRepository insuranceSubscriptionRepository;
-
-    @Autowired
-    private RestTemplate restTemplate;
 }
+//>>> Clean Arch / Inbound Adaptor
